4f58a06556a464fece24703b3028f80316fdbb6e
naoyamaguchi <naoyamaguchi0128@gmail.com>
2020-04-15 17:01:11 +0900

    add nfc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import jsQR, { QRCode } from 'jsqr';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,107 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular9-pwa';
+  @ViewChild('video', {static: true}) videoElm: ElementRef;
+  @ViewChild('canvas', {static: true}) canvasElm: ElementRef;
+
+  nfcInfo = '';
+
+  videoStart = false;
+  medias: MediaStreamConstraints = {
+    audio: false,
+    video: false,
+  };
+
+  constructor(
+    public dialog: MatDialog
+  ) { }
+
+  toggleVideoMedia() {
+    if (this.videoStart) {
+      this.stopVideo();
+    } else {
+      this.startVideo();
+    }
+  }
+
+  startVideo() {
+    this.medias.video = true;
+    navigator.mediaDevices.getUserMedia(this.medias).then(
+      (localStream: MediaStream) => {
+        this.videoElm.nativeElement.srcObject = localStream;
+        this.videoStart = true;
+        this.checkImage();
+      }
+    ).catch(
+      error => {
+        console.error(error);
+        this.videoStart = false;
+      }
+    );
+  }
+
+  stopVideo() {
+    this.medias.video = false;
+    this.videoElm.nativeElement.srcObject.getVideoTracks()[0].enabled = false;
+    this.videoElm.nativeElement.srcObject.getVideoTracks()[0].stop();
+    this.videoStart = false;
+  }
+
+  checkImage() {
+    const WIDTH = this.videoElm.nativeElement.clientWidth;
+    const HEIGHT = this.videoElm.nativeElement.clientHeight;
+    this.canvasElm.nativeElement.width  = WIDTH;
+    this.canvasElm.nativeElement.height = HEIGHT;
+
+    const ctx = this.canvasElm.nativeElement.getContext('2d') as CanvasRenderingContext2D;
+
+    ctx.drawImage(this.videoElm.nativeElement, 0, 0, WIDTH, HEIGHT);
+    const imageData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
+    const code = jsQR(imageData.data, imageData.width, imageData.height, { inversionAttempts: 'dontInvert' });
+
+    if (code) {
+        this.openDialog(code);
+    } else {
+        setTimeout(() => { this.checkImage(); }, 100);
+    }
+  }
+
+  openDialog(code: QRCode): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '360px',
+      data: {qrcode: code}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.checkImage();
+    });
+  }
+
+  getNFC() {
+    const reader = new NDEFReader();
+    async function startScan() {
+      await reader.scan();
+      reader.onreading = (e) => {
+        console.log(e.message);
+        this.nfcInfo = e.message;
+      };
+    }
+  }
+
+}
+
+@Component({
+  selector: 'app-dialog-component',
+  templateUrl: 'dialog-component.html',
+  styleUrls: [ './dialog-component.scss' ]
+})
+export class DialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: QRCode
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,50 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, DialogComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// material
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDialogModule } from '@angular/material/dialog';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     RouterModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
